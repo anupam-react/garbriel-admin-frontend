65584dfda57593164550f48f49d12a474e6d1912
anupam <anupamroy575@gamail.com>
2024-07-25 20:02:23 +0530

    update select item in filter

--- src/components/PromoCode/SelectItem.jsx
+++ src/components/PromoCode/SelectItem.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.scss";
-const SelectItem = () => {
+const SelectItem = ({onClose , show}) => {
+  
+  const popupRef = useRef();
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (show) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [show, onClose]);
   return (
-    <div className="retailer-container">
-      <div className="flex items-center px-6 h-12 input-loyalty">
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50 flex justify-center items-center">
+      <div ref={popupRef} className="bg-white p-4 rounded shadow-lg w-[300px]">
+      <div className="flex items-center px-6 h-16 input-loyalty">
         <img src="./image 2 (3).svg" alt="search" className="w-6 h-6" />
         <input type="text" className="search w-[150px]" placeholder="Search" />
       </div>
@@ -32,6 +52,7 @@ const SelectItem = () => {
             <p className="text-[#0070BC]">#ABCD12345</p>
           </div>
         </div>
+      </div>
       </div>
     </div>
   );
